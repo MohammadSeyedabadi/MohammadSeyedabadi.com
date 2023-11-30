e08e13658cc2cb851cbd3ec03027fa7a93f3a0c9
unknown <mhmd.sey.7@gmail.com>
2023-11-30 18:24:53 +0330

    #

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -1,10 +1,18 @@
 export const projectsList = [
   {
+    name: "MohammadSeyedabadi.com",
+    date: "2023",
+    slug: "MohammadSeyedabadi.com",
+    tagline:
+      "Personal website running on NextJS, React, and Node.js.",
+    highlight: true,
+  },
+  {
     name: "Flower-Shop-Template",
     date: "2023",
     slug: "Flower-Shop-Template",
     tagline:
-      "A Flower Shop Template made by using only HTML5, CSS3, Bootstrap and JavaScript",
+      "A Flower Shop Template made by using only HTML5, CSS3, Bootstrap and JavaScript.",
     highlight: true,
   },
 
@@ -19,7 +27,7 @@ export const projectsList = [
     name: "flower-shop",
     date: "2023",
     slug: "flower-shop",
-    tagline: "An almost complete flower shop website",
+    tagline: "An almost complete flower shop website.",
     highlight: true,
   },
 ];

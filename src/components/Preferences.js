9e3f1dd4598440407ef26044429a8137013952d3
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-01-31 10:31:16 +0330

    #

---
+++ src/components/Preferences.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import Toggle from "./layout/navigation/Toggle";
+
+export default function Preferences() {
+  useEffect(() => {
+    const openButton = document.querySelector("[data-open-modal]");
+    const closeButton = document.querySelector("[data-close-modal]");
+    const modal = document.querySelector("[data-modal]");
+
+    openButton.addEventListener("click", () => {
+      modal.showModal();
+    });
+
+    closeButton.addEventListener("click", () => {
+      modal.close();
+    });
+  }, []);
+  return (
+    <>
+      <button data-open-modal>Preferences</button>
+
+      <dialog data-modal>
+        <div className="Preferences--wrapper">
+          <Toggle />
+          
+          <button
+            data-close-modal
+            style={{
+              marginTop: "5rem",
+              alignSelf: "center",
+              textAlign: "start",
+            }}
+          >
+            Close
+          </button>
+        </div>
+      </dialog>
+    </>
+  );
+}

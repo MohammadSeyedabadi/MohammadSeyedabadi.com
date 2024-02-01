2bf03c1f43ad6fca17256e23c1361d2280517ffd
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-02-01 07:22:46 +0330

    #

--- src/app/[locale]/me/page.js
+++ src/app/[locale]/me/page.js
@@ -1,20 +1,17 @@
-import Head from 'next/head'
-import config from '@/@/utils/config'
-import TitleIcon from '@/@/assets/TitleIcon'
-import AboutSidebar from '@/@/components/AboutSidebar'
-import Hero from '@/@/components/Hero'
-import Link from 'next/link'
+import config from "@/utils/config";
+import TitleIcon from "@/assets/TitleIcon";
+import AboutSidebar from "@/components/AboutSidebar";
+import Hero from "@/components/Hero";
+import Link from "next/link";
+
+export const metadata = {
+  title: `About me | ${config.siteTitle}`,
+  description: `Read more about ${config.siteTitle}`,
+};
 
 export default function AboutMe() {
   return (
     <>
-      <Head>
-        <title>{`About me | ${config.siteTitle}`}</title>
-        <meta
-          name="description"
-          content={`Read more about ${config.siteTitle}`}
-        />
-      </Head>
       <div className="container">
         <div className="grid">
           <div className="article-content">
@@ -25,12 +22,12 @@ export default function AboutMe() {
                 <p>
                   I'm Mohammad! I'm a software developer working in Neyshabur.
                   Welcome to my spot on the web for my projects, tutorials and
-                  anything else I want to show the world. Check out the{' '}
+                  anything else I want to show the world. Check out the{" "}
                   <Link href="/projects">projects</Link> page to see a highlight
-                  of my open-source work, and the <Link href="/blog">blog</Link>{' '}
+                  of my open-source work, and the <Link href="/blog">blog</Link>{" "}
                   for my tutorials and more.
                 </p>
-                <h2 id="work-experience" style={{ position: 'relative' }}>
+                <h2 id="work-experience" style={{ position: "relative" }}>
                   <a
                     href="#work-experience"
                     aria-label="work experience permalink"
@@ -42,26 +39,29 @@ export default function AboutMe() {
                 </h2>
                 <p>
                   I won't bore you too much with my work history, though; that's
-                  what my{' '}
-                  <a href="/Resume/Mohammad Seyedabadi_Front-End Developer.pdf" download>
+                  what my{" "}
+                  <a
+                    href="/Resume/Mohammad Seyedabadi_Front-End Developer.pdf"
+                    download
+                  >
                     CV
                   </a>
-                  ,{' '}
+                  ,{" "}
                   <a
                     href="https://github.com/MohammadSeyedabadi"
                     target="_blank"
                     rel="noreferrer"
                   >
                     Github
-                  </a>{' '}
-                  and{' '}
+                  </a>{" "}
+                  and{" "}
                   <a
                     href="https://www.linkedin.com/in/mohammad-seyedabadi-397a61256/"
                     target="_blank"
                     rel="noreferrer"
                   >
                     LinkedIn
-                  </a>{' '}
+                  </a>{" "}
                   are for.
                 </p>
               </div>
@@ -71,5 +71,5 @@ export default function AboutMe() {
         </div>
       </div>
     </>
-  )
+  );
 }
--- src/components/AboutSidebar.js
+++ src/components/AboutSidebar.js
@@ -1,30 +1,31 @@
-import { useContext } from 'react'
-import ThemeContext from '@/store/theme-context'
-import Image from 'next/image'
+"use client";
+import { useContext } from "react";
+import ThemeContext from "@/store/theme-context";
+import Image from "next/image";
 
 export default function AboutSidebar() {
-  const { ariaActive } = useContext(ThemeContext)
+  const { ariaActive } = useContext(ThemeContext);
   return (
     <aside className="post-sidebar">
-      <div className="post-sidebar-card" style={{ width: 'fit-content' }}>
+      <div className="post-sidebar-card" style={{ width: "fit-content" }}>
         <h2>Me</h2>
         <Image
-            src="/images/me2.jpg"
-            width={256}
-            height={256}
-            alt="Mohammad"
-            quality={100}
-            style={ariaActive ? null : {display:"none"}}
-          />
-          <Image
-            src="/images/me1.jpg"
-            width={256}
-            height={256}
-            alt="Mohammad"
-            quality={100}
-            style={ariaActive ? {display:"none"} : null}
-          />
+          src="/images/me2.jpg"
+          width={256}
+          height={256}
+          alt="Mohammad"
+          quality={100}
+          style={ariaActive ? null : { display: "none" }}
+        />
+        <Image
+          src="/images/me1.jpg"
+          width={256}
+          height={256}
+          alt="Mohammad"
+          quality={100}
+          style={ariaActive ? { display: "none" } : null}
+        />
       </div>
     </aside>
-  )
+  );
 }

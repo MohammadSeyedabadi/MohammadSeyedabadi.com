d9b86f6ffaf1389ec3c56dfddb8f18d33ac319ab
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-02-01 11:11:54 +0330

    #

--- src/app/[locale]/blog/page.js
+++ src/app/[locale]/blog/page.js
@@ -1,31 +1,29 @@
-import Head from 'next/head'
-import config from '@/@/utils/config'
-import SidebarLayout from '@/@/components/layout/SidebarLayout'
-import Hero from '@/@/components/Hero'
-import Posts from '@/@/components/Posts'
-import { getAllPosts } from '@/@/utils/posts-util'
+import Head from "next/head";
+import config from "@/utils/config";
+import SidebarLayout from "@/components/layout/SidebarLayout";
+import Hero from "@/components/Hero";
+import Posts from "@/components/Posts";
+import { getAllPosts } from "@/utils/posts-util";
 
-export default function Blog(props) {
+export const metadata = {
+  title: `Writing | ${config.siteTitle}`,
+  description: "A list of all my posts",
+};
+
+export default async function Blog() {
+  const allPosts = await getData();
   return (
     <>
-      <Head>
-        <title>{`Writing | ${config.siteTitle}`}</title>
-        <meta name="description" content="A list of all my posts" />
-      </Head>
       <SidebarLayout>
         <Hero title="Writing" />
-        <Posts allPosts={props.allPosts} />
+        <Posts allPosts={allPosts} />
       </SidebarLayout>
     </>
-  )
+  );
 }
 
-export function getStaticProps() {
-  const allPosts = getAllPosts()
+export async function getData() {
+  const allPosts = getAllPosts();
 
-  return {
-    props: {
-      allPosts: allPosts,
-    },
-  }
+  return allPosts;
 }

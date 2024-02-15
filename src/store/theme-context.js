f8f8d293d144a192d20a790b0f158b10740d1701
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-02-15 15:04:32 +0330

    #

--- src/store/theme-context.js
+++ src/store/theme-context.js
@@ -1,69 +1,67 @@
 "use client";
 
 import { createContext, useState, useEffect } from "react";
-import { setTheme, keepTheme } from "@/utils/themes";
 
 const ThemeContext = createContext();
 
 export function ThemeContextProvider(props) {
-  const [className, setClassName] = useState("theme-dark");
-  useEffect(() => {
-    keepTheme(setClassName);
-  }, [setClassName]);
   // false = dark mode because of the way I wrote the CSS
   const [active, setActive] = useState(false);
   // the opposite, for screenreaders
   const [ariaActive, setAriaActive] = useState(true);
 
-  let theme;
-  if (typeof window !== "undefined") {
-    theme = localStorage.getItem("theme");
-  }
+  let theme = localStorage.getItem("theme");
 
-  const changeThemeAndToggle = () => {
+  function changeThemeAndToggle() {
     if (localStorage.getItem("theme") === "theme-dark") {
-      setTheme("theme-light", setClassName);
+      localStorage.setItem("theme", "theme-light");
       setActive(true);
       setAriaActive(false);
     } else {
-      setTheme("theme-dark", setClassName);
+      localStorage.setItem("theme", "theme-dark");
       setActive(false);
       setAriaActive(true);
     }
-  };
+  }
 
-  const handleOnClick = () => {
-    if (typeof window !== "undefined") {
+  function handleOnClick() {
+    changeThemeAndToggle();
+  }
+
+  function handleKeypress(e) {
+    if (e.code === "Enter") {
       changeThemeAndToggle();
     }
-  };
-
-  const handleKeypress = (e) => {
-    if (typeof window !== "undefined") {
-      if (e.code === "Enter") {
-        changeThemeAndToggle();
-      }
-    }
-  };
+  }
 
   useEffect(() => {
     if (theme === "theme-dark") {
       setActive(false);
       setAriaActive(true);
-      let root = document.querySelector(":root");
-      // root.classList.add('dark');
-      root.setAttribute("data-theme", "dark");
+      document.querySelector(":root").setAttribute("data-theme", "dark");
     } else if (theme === "theme-light") {
       setActive(true);
       setAriaActive(false);
-      let root = document.querySelector(":root");
-      // root.classList.remove('dark');
-      root.setAttribute("data-theme", "light");
+      document.querySelector(":root").setAttribute("data-theme", "light");
+    } else {
+      let prefersLightTheme = window.matchMedia(
+        "(prefers-color-scheme: light)"
+      );
+      if (prefersLightTheme.matches) {
+        localStorage.setItem("theme", "theme-light");
+        setActive(true);
+        setAriaActive(false);
+        document.querySelector(":root").setAttribute("data-theme", "light");
+      } else {
+        localStorage.setItem("theme", "theme-dark");
+        setActive(false);
+        setAriaActive(true);
+        document.querySelector(":root").setAttribute("data-theme", "dark");
+      }
     }
   }, [theme]);
 
   const context = {
-    setClassName,
     active,
     ariaActive,
     handleOnClick,

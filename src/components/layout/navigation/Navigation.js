b6b4dac12081c8df16fde592fa82705cb9c29388
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-03-20 12:12:31 +0330

    #

--- src/components/layout/navigation/Navigation.js
+++ src/components/layout/navigation/Navigation.js
@@ -54,7 +54,7 @@ export default function Navigation() {
               </span>
             </Link>
           </span>
-          <span
+          {/* <span
             className={
               pathname === "/fa/contact" || pathname === "/en/contact"
                 ? "active--link"
@@ -66,7 +66,7 @@ export default function Navigation() {
                 {lang === "en" ? "Contact" : "تماس"}
               </span>
             </Link>
-          </span>
+          </span> */}
 
           <span
             className={

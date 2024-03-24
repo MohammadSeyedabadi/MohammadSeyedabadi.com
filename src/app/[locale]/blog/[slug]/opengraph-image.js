8738de492864f3f6c67041089e922d6ef2884032
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-03-24 12:37:35 +0330

    #

---
+++ src/app/[locale]/blog/[slug]/opengraph-image.js
@@ -0,0 +1,53 @@
+import { ImageResponse } from "next/server";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image({ params }) {
+  // Font
+  const interSemiBold = fetch(
+    new URL("/public/fonts/fonts/ttf/Vazirmatn-Black.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {params.slug}
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Vazirmatn",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
+  );
+}

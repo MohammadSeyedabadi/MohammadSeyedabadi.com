21aad13118ccc91994789ce0aa80a2222a4f94a7
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-05-21 07:49:04 +0330

    Lunched newsletter

--- src/components/PostSidebar.js
+++ src/components/PostSidebar.js
@@ -1,10 +1,7 @@
 "use client";
-// import { useContext } from "react";
-// import ThemeContext from "@/store/theme-context";
 import { Link } from "@/navigation";
 
 export default function PostSidebar({ metaData, translation }) {
-  // const { ariaActive } = useContext(ThemeContext);
   const { lang, title, slug, image, date, category, tags } = metaData;
   const formattedDate = new Date(date).toLocaleDateString(
     lang === "fa" ? "fa-IR" : "en-US",
@@ -61,33 +58,20 @@ export default function PostSidebar({ metaData, translation }) {
 
       <div className="post-sidebar-card">
         <h2>{AboutMe}</h2>
-        {/* <img
-          src="/images/me2.jpg"
-          alt="Mohammad"
-          className="sidebar-avatar"
-          style={ariaActive ? null : { display: "none" }}
-        />
-        <img
-          src="/images/me1.jpg"
-          alt="Mohammad"
-          className="sidebar-avatar"
-          style={ariaActive ? { display: "none" } : null}
-        /> */}
         <p>
           {PostSideBarDescOne} <Link href="/me">{PostSideBarDescTwo}</Link>
         </p>
         <p>{PostSideBarDescThree}</p>
       </div>
 
-      <div className="post-sidebar-card" style={{ opacity: "0.5" }}>
+      <div className="post-sidebar-card">
         <h2>{Newsletter}</h2>
         <p>{PostSideBarDescFour}</p>
-        <p style={{ pointerEvents: "none", cursor: "default" }}>
+        <p>
           <a
-            href="/"
+            href="https://mohammadseyedabadi.substack.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="button highlighted"
           >
             {SubscribeToTheNewsletter}
           </a>

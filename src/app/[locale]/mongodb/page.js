09dcc355a9cb0f2b396f936079ea294041a98948
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-08-25 18:35:20 +0330

    #

--- src/app/[locale]/mongodb/page.js
+++ src/app/[locale]/mongodb/page.js
@@ -1,18 +1,9 @@
 import clientPromise from "../../../utils/mongodb";
 
 export default async function page() {
-  const { props: allNotesTitle } = await getData();
-  console.log(allNotesTitle);
-  return (
-    <div>
-      <div>my notes</div>
-      <div>ID : {allNotesTitle.allNotesTitle[0]._id}</div>
-      <div>CREATED AT : {allNotesTitle.allNotesTitle[0].createdAt}</div>
-      <div>SLUG : {allNotesTitle.allNotesTitle[0].slug}</div>
-      <div>TITLE : {allNotesTitle.allNotesTitle[0].title}</div>
-      <div>DESC : {allNotesTitle.allNotesTitle[0].desc}</div>
-    </div>
-  );
+  const allNotesTitle = await getData();
+
+  return <div className="container"></div>;
 }
 
 export async function getData() {
@@ -25,13 +16,15 @@ export async function getData() {
       .project({ title: 1, slug: 1, _id: 0 })
       .toArray();
 
-    return {
-      props: { allNotesTitle: JSON.parse(JSON.stringify(allNotesTitle)) },
-    };
+    return allNotesTitle;
+    // return {
+    //   props: { allNotesTitle: JSON.parse(JSON.stringify(allNotesTitle)) },
+    // };
   } catch (e) {
     console.error(e);
-    return {
-      props: { allNotesTitle: [] },
-    };
+    return [];
+    // return {
+    //   props: { allNotesTitle: [] },
+    // };
   }
 }

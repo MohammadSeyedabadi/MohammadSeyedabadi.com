bac115d554641d37f7b837f385a294446415c6ed
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2024-09-01 22:21:24 +0330

    #

--- src/app/[locale]/categories/[category]/page.js
+++ src/app/[locale]/categories/[category]/page.js
@@ -26,7 +26,7 @@ export async function generateMetadata({ params }) {
 
 export default async function category({ params }) {
   let { locale, category } = params;
-  console.log(category)
+  
   locale == "fa" && (category = decodeURI(category));
   let all_posts_preview_data;
 
@@ -35,7 +35,7 @@ export default async function category({ params }) {
       locale,
       category
     );
-    console.log(all_posts_preview_data)
+    
   } catch (error) {
     console.error(
       `Failed To Fetch All Posts Meta Data In /categories/[category]/page.js. Error Message : ${error}`

731a76a08ba699e5e1b841d19bddb9add0574286
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2025-02-12 15:17:25 +0330

    #

--- src/components/PostSidebar.js
+++ src/components/PostSidebar.js
@@ -29,8 +29,8 @@ export default function PostSidebar({ metaData, translation }) {
         <img src={image} alt={title} className="max-w-36 mb-2 mx-auto" />
       </div>
 
-      <div className="p-6 bg-neutral-100/45 rounded-xl border-2 border-solid border-neutral-300 dark:bg-neutral-800 dark:border-neutral-500">
-        <h2 className="text-base font-bold uppercase text-neutral-800 dark:text-neutral-100">{PostDetails}</h2>
+      <div className="px-10 py-7 bg-neutral-100/45 rounded-xl border-2 border-solid border-neutral-300 dark:bg-neutral-800 dark:border-neutral-500">
+        <h2 className="text-base uppercase text-neutral-800 dark:text-neutral-100">{PostDetails}</h2>
         <ul className="mb-3 list-disc list-outside ms-5 text-sm text-neutral-800 dark:text-neutral-300">
           <li>
             <strong>{Published}:</strong> {createdAtformattedDate}

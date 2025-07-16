70cfd3a9fc6d398a7dcea4f299344bb7d40cc927
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2025-07-16 16:43:22 +0330

    #

--- src/app/[locale]/blog/page.js
+++ src/app/[locale]/blog/page.js
@@ -32,6 +32,12 @@ export default async function Page(props) {
             <h1 className="text-5xl font-bold text-neutral-800 dark:text-neutral-100 mb-3">
               {t("Blog")}
             </h1>
+            <Link
+              href="/tags"
+              className="text-lg hover:underline inline-block active:scale-95 text-indigo-500 dark:text-indigo-300"
+            >
+              {t("ViewAllTags")}
+            </Link>
           </header>
           <section>
             {sortedPosts.map((group) => (
@@ -44,7 +50,7 @@ export default async function Page(props) {
                     <Link
                       key={post.slug}
                       href={`/${post.slug}`}
-                      className="mb-4 flex items-center justify-between gap-3 font-medium py-1 px-3 bg-neutral-100/45 rounded-xl border-2 border-neutral-300 hover:border-rose-500 dark:bg-neutral-800 dark:text-neutral-300 dark:border-neutral-500 dark:hover:text-neutral-100 dark:hover:border-rose-300 active:scale-95 hover:visited:border-indigo-500 hover:dark:visited:border-indigo-300"
+                      className="mb-4 flex items-center justify-between gap-3 font-medium py-1 px-3 bg-neutral-100/45 rounded-xl border-2 border-neutral-300 dark:bg-neutral-800 dark:text-neutral-300 dark:border-neutral-500 dark:hover:text-neutral-100 active:scale-95 hover:border-indigo-500 hover:dark:border-indigo-300"
                     >
                       <h3 className="text-lg">{post.title}</h3>
                       <time className="hidden lg:inline font-mono">
@@ -61,35 +67,6 @@ export default async function Page(props) {
         </section>
         <div className="sm:col-span-2" />
       </div>
-      //   <div className="md:flex md:justify-between bg-white/90 p-4 rounded md:max-h-full md:overflow-auto h-full">
-      //     <div>
-      //       <h1 className="text-2xl font-bold mb-6">📅 English posts</h1>
-
-      //       {sortedPosts.map((group) => (
-      //         // const formattedcreatedatmonthday = ""
-      //         <section key={group.year} className="mb-8">
-      //           <h2 className="text-xl font-semibold text-gray-800 mb-2">
-      //             {locale == "en" ? group.year : group.posts[0].fadigityear}
-      //           </h2>
-      //           <ul className="space-y-1">
-      //             {group.posts.map((post) => (
-      //               <li key={post.slug}>
-      //                 <Link
-      //                   href={`/${post.title}`}
-      //                   className="text-blue-600 hover:underline flex active:scale-95"
-      //                 >
-      //                   {locale == "en"
-      //                     ? post.formattedcreatedatmonthday
-      //                     : post.faformattedcreatedatmonthday}
-      //                   — {post.title}
-      //                 </Link>
-      //               </li>
-      //             ))}
-      //           </ul>
-      //         </section>
-      //       ))}
-      //     </div>
-      //   </div>
     );
   } catch (error) {
     console.error("Database Error:", error);

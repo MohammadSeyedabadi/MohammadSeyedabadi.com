e45148a35472c2487a8f9db01395f301e43b6033
Mohammad Seyedabadi <mhmd.sey.7@gmail.com>
2025-07-16 17:35:28 +0330

    #

--- src/app/[locale]/tags/page.js
+++ src/app/[locale]/tags/page.js
@@ -2,8 +2,7 @@ import { sql } from "@/data/data";
 import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 import { TagsSkeleton } from "@/components/skeletons";
-import ArrangeTags from "@/components/ArrangeTags";
-import { getAllTags } from "@/posts/tags";
+import { Link } from "@/i18n/routing";
 
 export async function generateMetadata() {
   const t = await getTranslations("Config");
@@ -22,111 +21,61 @@ export async function generateMetadata() {
 export default async function page(props) {
   const params = await props.params;
   const { locale } = params;
-  const tagTable = locale == "en" ? "entags" : "fatags";
-  const tags = await sql`
-  SELECT name FROM ${sql(tagTable)} ORDER BY name ASC;
-`;
-  const groupedTags = tags.reduce((acc, tag) => {
-    const firstLetter = tag.name[0].toUpperCase();
-    if (!acc[firstLetter]) acc[firstLetter] = [];
-    acc[firstLetter].push(tag.name);
-    return acc;
-  }, {});
   const t = await getTranslations("Tags");
   return (
-    // <div className="max-w-6xl mx-auto px-4 sm:px-8">
-    //   <h1 className="text-5xl font-bold text-neutral-800 dark:text-neutral-100">
-    //     {t("AllTags")}
-    //   </h1>
-    //   <section className="my-12 md:my-8">
-    //     <Suspense fallback={<TagsSkeleton />}>
-    //       <FetchTags locale={locale} />
-    //     </Suspense>
-    //   </section>
-    // </div>
-    <>
-      {Object.keys(groupedTags)
-        .sort((a, b) => a.localeCompare(b, locale, { sensitivity: "base" }))
-        .map((letter) => (
-          <div key={letter} className="mb-6">
-            <h2 className="text-xl font-bold text-gray-700">{letter}</h2>
-            <ul className="mt-2 space-y-1">
-              {groupedTags[letter].map((tag) => (
-                <li key={tag}>
-                  <a
-                    href={`/tags/${tag}`}
-                    className="text-blue-600 hover:underline"
-                  >
-                    {tag}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-    </>
+    <div className="max-w-6xl mx-auto px-4 sm:px-8">
+      <h1 className="text-5xl font-bold text-neutral-800 dark:text-neutral-100">
+        {t("AllTags")}
+      </h1>
+      <section className="my-12 md:my-8">
+        <Suspense fallback={<TagsSkeleton />}>
+          <FetchTags locale={locale} />
+        </Suspense>
+      </section>
+    </div>
   );
 }
 
 export async function FetchTags({ locale }) {
   try {
-    const client = await clientPromise;
-    const db = client.db("notes");
+    const tagTable = locale == "en" ? "entags" : "fatags";
+    const tags = await sql`
+  SELECT name FROM ${sql(tagTable)} ORDER BY name ASC;
+`;
+    const groupedTags = tags.reduce((acc, tag) => {
+      const firstLetter = tag.name[0].toUpperCase();
+      if (!acc[firstLetter]) acc[firstLetter] = [];
+      acc[firstLetter].push(tag.name);
+      return acc;
+    }, {});
 
-    let allTags = await db
-      .collection(locale == "en" ? "enTags" : "faTags")
-      .find({}, { projection: { _id: 0 } })
-      .toArray();
-
-    let allTagsCopy = JSON.parse(JSON.stringify(allTags[0])); // copy the value because of deleting in for loop
-
-    for (const key in allTags[0]) {
-      if (allTags[0][key].length === 0) {
-        delete allTagsCopy[key];
-      }
-    }
-
-    const allTagsInLocal = await getAllTags(locale);
-
-    const mergeObjects = (allTagsCopy, allTagsInLocal) => {
-      const merged = {};
-
-      const addWordsToMerged = (obj) => {
-        for (const [key, words] of Object.entries(obj)) {
-          words.forEach((word) => {
-            const startingLetter = word[0].toUpperCase();
-            if (!merged[startingLetter]) {
-              merged[startingLetter] = [];
-            }
-            merged[startingLetter].push(word);
-          });
-        }
-      };
-
-      addWordsToMerged(allTagsCopy);
-      addWordsToMerged(allTagsInLocal);
-
-      // // Remove duplicates
-      // for (const key in merged) {
-      //   merged[key] = [...new Set(merged[key])];
-      // }
-      // return merged;
-      /////////////////////////////////////////////
-      // Remove duplicates and sort keys
-      const sortedMerged = {};
-      Object.keys(merged)
-        .sort()
-        .forEach((key) => {
-          sortedMerged[key] = [...new Set(merged[key])];
-        });
-
-      return sortedMerged;
-    };
-
-    const mergedResult = mergeObjects(allTagsCopy, allTagsInLocal);
-
-    return <ArrangeTags allTags={mergedResult} notes={true} />;
-  } catch (e) {
-    throw new Error(e);
+    return (
+      <>
+        {Object.keys(groupedTags)
+          .sort((a, b) => a.localeCompare(b, locale, { sensitivity: "base" }))
+          .map((letter) => (
+            <div key={letter} className="mb-7">
+              <h3 className="text-3xl font-bold text-neutral-800 dark:text-neutral-100 mb-1">
+                {letter}
+              </h3>
+              <ul className="flex flex-wrap items-center gap-3">
+                {groupedTags[letter].map((tag) => (
+                  <li key={tag}>
+                    <Link
+                      href={`/tags/${tag}`}
+                      className="text-sm font-medium py-1 px-3 bg-neutral-100/45 rounded-xl border-2 border-neutral-300 tracking-wider dark:bg-neutral-800 dark:text-neutral-300 dark:border-neutral-500 dark:hover:text-neutral-100 active:scale-95 hover:border-indigo-500 hover:dark:border-indigo-300"
+                    >
+                      {tag}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+      </>
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch tags");
   }
 }
